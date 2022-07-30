f51d99a8d0de0e45e1a24a72fdbfd4249a8d2813
lemon203213 <lemon203213@gmail.com>
2022-07-30 13:50:14 +0900

    요

---
+++ src/main/java/com/hanghae/helog/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.hanghae.helog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CommentService {
+
+}
