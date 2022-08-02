0114dfa0a2e098faedcdaca5dae135a1e4016a2d
GiwoongBang <bgw6713@naver.com>
2022-08-02 18:08:05 +0900

    * 게시글 상세 조회 DTO

---
+++ src/main/java/com/hanghae/helog/dto/post/PostDetailResponseDto.java
@@ -0,0 +1,30 @@
+package com.hanghae.helog.dto.post;
+
+import com.hanghae.helog.domain.Post;
+import lombok.Getter;
+
+@Getter
+public class PostDetailResponseDto {
+
+    private Long post_id;           // 게시글 ID
+    private String title;           // 게시글 제목
+    private String subTitle;        // 게시글 서브 제목
+    private String createdAt;       // 작성 시간
+    private Long user_id;           // 사용자 아이디
+    private int commentCount;       // 댓글 개수
+    private String thumbnail;       // 이미지 파일 저장 장소
+    private String url;             // 게시글 URL
+
+
+    public PostDetailResponseDto(Post post, int commentCount) {
+        this.post_id = post.getPost_id();
+        this.title = post.getTitle();
+        this.subTitle = post.getSubTitle();
+        this.createdAt = post.getCreatedAt();
+        this.user_id = getUser_id();
+        this.commentCount = commentCount;
+        this.thumbnail = post.getThumbnail();
+        this.url = post.getUrl();
+    }
+
+}
