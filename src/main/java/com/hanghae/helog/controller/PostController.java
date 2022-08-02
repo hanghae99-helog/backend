224469061fc9a9bebc76c132eedba2f37d6c7e40
GiwoongBang <bgw6713@naver.com>
2022-08-02 18:06:07 +0900

    * 게시글 수정, 삭제 ** 게시글 상세 조회, 전체 조회 추가

--- src/main/java/com/hanghae/helog/controller/PostController.java
+++ src/main/java/com/hanghae/helog/controller/PostController.java
@@ -1,9 +1,17 @@
 package com.hanghae.helog.controller;
 
+import com.hanghae.helog.domain.Post;
+import com.hanghae.helog.dto.post.AllPostResponseDto;
 import com.hanghae.helog.dto.post.PostCreateReqeustDto;
+import com.hanghae.helog.dto.post.PostDetailResponseDto;
+import com.hanghae.helog.dto.post.PostEditRequestDto;
 import com.hanghae.helog.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,6 +20,19 @@ public class PostController {
     private final PostService postService;
 
 
+    // 게시글 전체 조회
+    @GetMapping("/api/list")
+    public List<AllPostResponseDto> getAllPosts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+    ) {
+        return postService.getAllPosts(page, size, sortBy, isAsc);
+    }
+
+
+    // 게시글 작성
     @PostMapping("/api/posting")
     public void createPost(@AuthenticationPrincipal UserDetailsImpl userDetails,
                            @RequestBody PostCreateReqeustDto postCreateReqeustDto) {
@@ -19,12 +40,31 @@ public class PostController {
         postService.createPost(userDetails, postCreateReqeustDto);
     }
 
-    @PutMapping("/api/posting/{postid}")
-    public void editPost(@PathVariable Long postId,
-                         @RequestBody EditPostRequestDto requestDto,
-                         @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        postService.editPost(postId, requestDto, userDetails);
+    // 게시글 상세조회
+    @GetMapping("/api/postiong/{url}")
+    public PostDetailResponseDto getPostDetails(@RequestParam String url) {
+
+        return postService.getPostDetails(url);
+    }
+
+
+    // 게시글 수정
+    @PutMapping("/api/posting/{post_id}")
+    public ResponseEntity<?> editPost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                      @PathVariable Long post_id,
+                                      @RequestBody PostEditRequestDto postEditRequestDto) {
+
+        return postService.editPost(userDetails, post_id, postEditRequestDto);
+    }
+
+
+    // 게시글 삭제
+    @DeleteMapping("api/posting/{post_id}")
+    public ResponseEntity<?> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                           @PathVariable Long post_id) {
+
+        return postService.deletePost(userDetails, post_id);
     }
 
 }
